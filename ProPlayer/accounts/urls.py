fb3706ee7462f87736a0f7a253c21495647d6e39
NagibSuaid <nagibmoura@gmail.com>
2020-12-02 21:23:16 -0300

    sistema de contas e login feito, faltando views para registro e perfil

---
+++ ProPlayer/accounts/urls.py
@@ -0,0 +1,37 @@
+
+from django.urls import path
+from . import views
+from django.urls.base import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.views.generic.edit import UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordResetView ,PasswordResetConfirmView, PasswordResetDoneView
+app_name = 'accounts'
+
+urlpatterns = [
+    path('register', views.register, name='register'),
+    path('login', LoginView.as_view(), name='login'),
+    path('profile', views.profile, name='profile'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('change-password', PasswordChangeView.as_view(success_url = reverse_lazy('change-password-done')), name='change-password'),
+    path('change-password-done', PasswordChangeDoneView.as_view()),
+    path('update',  UpdateView.as_view(
+                        success_url=reverse_lazy('sec-paginaSecreta'),
+                        model=User,
+                        fields=[
+                            'first_name',
+                            'last_name',
+                            'email',
+                        ]
+                    ), name='update'),
+    path('reset', PasswordResetView.as_view(
+            success_url=reverse_lazy('reset-done'),
+            from_email='webmaster_do_site@aqui.com.br', #???
+            subject_template_name='accounts/password_reset_subject.txt',
+            email_template_name='accounts/password_reset_email.html',
+         ), name='reset'),
+    path('reset-confirm', PasswordResetConfirmView.as_view(), name='reset-confirm'),
+    path('reset-done', PasswordResetDoneView.as_view(), name='reset-done'),
+
+]
--- ProPlayer/accounts/views.py
+++ ProPlayer/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def profile(request):
+    return render(request, 'accounts/register.html')
