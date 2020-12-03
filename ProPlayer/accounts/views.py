1b5cbf410f3a8c99d94573c229d828d56b5ee760
NagibSuaid <nagibmoura@gmail.com>
2020-12-02 21:25:22 -0300

    decorator de login required na view do perfil

--- ProPlayer/accounts/views.py
+++ ProPlayer/accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
     return render(request, 'accounts/register.html')
 
-
+@login_required
 def profile(request):
     return render(request, 'accounts/register.html')
