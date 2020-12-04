a70090f4217d161d72f9f9000f816327674e96b6
NagibSuaid <nagibmoura@gmail.com>
2020-12-03 23:03:19 -0300

    verificação de usuário existente no registro via AJAX

--- ProPlayer/accounts/urls.py
+++ ProPlayer/accounts/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('change-password/', PasswordChangeView.as_view(success_url = reverse_lazy('change-password-done')), name='change-password'),
     path('change-password-done/', PasswordChangeDoneView.as_view()),
-    path('update/',  UpdateView.as_view(
+    path('update/<int:pk>',  UpdateView.as_view(
                         success_url=reverse_lazy('sec-paginaSecreta'),
                         model=User,
                         fields=[
@@ -31,7 +31,8 @@ urlpatterns = [
             subject_template_name='accounts/password_reset_subject.txt',
             email_template_name='accounts/password_reset_email.html',
          ), name='reset'),
-    path('reset-confirm/', PasswordResetConfirmView.as_view(), name='reset-confirm'),
+    path('reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='reset-confirm'),
     path('reset-done/', PasswordResetDoneView.as_view(), name='reset-done'),
+    path('verify_username/', views.verify_username, name='verify-username'),
 
 ]
--- ProPlayer/accounts/views.py
+++ ProPlayer/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 
 def register(request):
@@ -8,3 +9,12 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'accounts/register.html')
+
+
+def verify_username(request):
+    username = request.GET.get('username','')
+
+    response = {
+        'exists' : User.objects.filter(username__iexact=username).exists()
+    }
+    return JsonResponse(response)
