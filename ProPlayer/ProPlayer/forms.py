3a1d18727f8485ab28f87eb0966212f5e1291960
NagibSuaid <nagibmoura@gmail.com>
2020-12-05 21:51:05 -0300

    nome dos times é exibido na criação de players

--- ProPlayer/ProPlayer/forms.py
+++ ProPlayer/ProPlayer/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from ProPlayer.models import Player, Game, Team
 
+class CustomModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self,obj):
+        return obj.name
+
 class PlayerModel2Form(forms.ModelForm):
     class Meta:
         model = Player
         fields = '__all__'
 
-    team = forms.ModelChoiceField(queryset=None)
+    team = CustomModelChoiceField(queryset=None)
 
     game_options = lambda  : tuple(
         [(game.pk,game.title) for game in list(Game.objects.all())]
